88d9018a7871c2e2bde280f9bada80c6284d88f9
baijiahao123 <782537644@qq.com>
2024-07-27 22:00:58 +0800

    daily save

---
+++ src/main/scala/IFU/PCReg.scala
@@ -0,0 +1,26 @@
+package riscv32
+
+import chisel3._
+import chisel3.util._
+
+class PCRegIO extends Bundle{
+    val resultBranch = Input(Bool())
+    val pcJump       = Input(Bool())
+    val resultpc     = Input(UInt(32.W))
+}
+
+class PCReg extends Module {
+  val io = IO(new PCRegIO())
+    
+    val regPC = RegInit(0x80000000.U(32.W))
+    when(io.pcJump || io.resultBranch){
+        regPC := io.resultpc
+    }.otherwise{
+        regPC := regPC + 4.U
+    }
+
+    val pc := regPC
+}
+
+  
+
